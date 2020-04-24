3d381781fa82e9751a9fa714194459963359e426
Alhad Bhadekar <alhad.bhadekar@gmail.com>
2020-04-24 13:16:32 -0400

    Django Project commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\alhad\Desktop\Work\Python_Virtual_Environments\DjangoZerotoHero\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
